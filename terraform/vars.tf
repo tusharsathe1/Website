ca116cdabc70be5c7df2d91e5c60f123822fe2d6
tusharsathe1 <68548633+tusharsathe1@users.noreply.github.com>
2020-10-18 10:30:57 -0400

    Create vars.tf

---
+++ terraform/vars.tf
@@ -0,0 +1,9 @@
+variable "AWS_ACCESS_KEY" {}
+variable "AWS_SECRET_KEY" {}
+variable "region" {}
+variable "main_domain" {}
+variable "sub_domain" {}
+variable "log_bucket" {}
+variable "function_name" {
+  type = string
+}
